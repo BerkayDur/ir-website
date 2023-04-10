6d29a5eae46902bb5a4213b6baa98ec4f5e03612
BerkayDur <95105235+BerkayDur@users.noreply.github.com>
2023-04-10 21:41:45 +0100

    Title update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import "./header.css"
 
 function Header(){
     return <header className="header">
-                <h1 className="h1">Information Retrieval Group 32</h1>
+                <h1 className="h1">Information Retrieval Assignment</h1>
             </header>
 }
 
